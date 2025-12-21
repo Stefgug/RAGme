62e2522f559b7bb2b69eff087ebf812e1b6a8afe
Stephane Guren <stephane.guren@gmail.com>
2025-12-21 19:35:39 +0100

    Update project configuration and add embedding functionality
    - Modify .gitignore to include additional files and directories
    - Create docker-compose.yml for Qdrant service setup
    - Add embedding_qstoring.py for document embedding and storage in Qdrant
    - Update pyproject.toml to include new dependencies for embeddings
    - Remove obsolete uv.lock file

---
+++ embedding/embedding_qstoring.py
@@ -0,0 +1,47 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+
+from tqdm import tqdm
+
+
+documents = PyPDFLoader("data/sample_pdf.pdf").load()
+
+
+# Embedding
+embeddings = HuggingFaceEmbeddings(
+    model_name="BAAI/bge-large-en-v1.5"
+)
+
+embeddings_list = embeddings.embed_documents([doc.page_content for doc in documents])
+
+# Store in Qdrant
+qdrant_client = QdrantClient("localhost:6333")
+
+try:
+    collection = qdrant_client.get_collection("documents_collection")
+    print(f"Collection already exists")
+except:
+    # Collection doesn't exist, create it
+    qdrant_client.create_collection(
+        collection_name="documents_collection",
+        vectors_config=VectorParams(size=len(embeddings_list[0]), distance=Distance.COSINE)
+    )
+    print("Collection created successfully")
+
+for idx, vector in tqdm(enumerate(embeddings_list), total=len(embeddings_list)):
+    qdrant_client.upsert(
+        collection_name="documents_collection",
+        points=[
+            {
+                "id": idx,
+                "vector": vector,
+                "payload": {
+                    "text": documents[idx].page_content
+                }
+            }
+        ]
+    )
+print("Documents embedded and stored in Qdrant successfully")
